8ae382ad972092e5d3cfbe155e494900a3ddd4ae
truncs <sarawgi.aditya@gmail.com>
2013-01-11 17:30:43 -0500

    prototype bloom filters with paritioning

--- korine/bloom_filter.py
+++ korine/bloom_filter.py
@@ -2,7 +2,7 @@ import math
 import hashlib
 
 try:
-    import bitarray
+    from bitarray import bitarray
 except ImportError:
     raise ImportError("requires bitarray")
 
@@ -14,10 +14,34 @@ except ImportError:
 
 class BloomFilter(object):
     
-    def __init__(self, capacity, error_rate=0.001):
+    def __init__(self, capacity=100000, error_rate=0.001):
         """ BloomFilter is a probabilistic datastructure which are helpful in
         approximate membership queries. They only suffer from type 1 false
         positive errors and the error rate can be tuned to specific requirements.
         """
+        self.total_size = 20000
+        self.size = 2000
+        self.k = 10
+        self.bitarrays = []
+        self.h1 = pyhash.fnv1a_64()
+        self.h2 = pyhash.murmur2_x86_64b()
+        for i in self.k:
+            b = bitarray(size, endian='little')
+            b.setall(False)
+            self.bitarrays.append(b)
+
+    def insert(self, element):
+        unicode_element = unicode(element)
+        element_hash1 = h1(unicode_element)
+        element_hash2 = h2(unicode_element)
+        for i in range(self.k):
+            bit_no = (element_hash1 + i*element_hash2) % self.size
+            self.bitarrays[i][bit_no] = True
         
         
+    def check(self, element):
+        unicode_element = unicode(element)
+        element_hash1 = h1(unicode_element1)
+        element_hash2 = h2(unicode_element2)
+        
+        return all([self.bitarrays[i][(element_hash1 + i*element_hash2) % self.size] for i in range(k)])
